7cf4e867fc9495980da308d6512828dc09d3cb13
spineda2019 <spineda@wpi.edu>
2023-10-17 13:18:09 -0400

    Added test 1

--- TestingMachineLearningLibrary/Main.cpp
+++ TestingMachineLearningLibrary/Main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
+
 #include "Matrix.h"
+
+bool test_vector_constructor() {
+  try {
+    // Good
+    std::vector<std::vector<int>> data{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    tensor_math::Matrix<int> test = tensor_math::Matrix<int>(data);
+  } catch (BadDataConstructorException& e) {
+    std::cout << "Unexpected CTOR failure: Test 1 failed" << std::endl;
+    return false;
+  }
+
+  try {
+    // Bad
+    std::vector<std::vector<int>> bad_data{{0, 0, 0}, {0, 0}, {0}};
+    tensor_math::Matrix<int> bad_test = tensor_math::Matrix<int>(bad_data);
+  } catch (BadDataConstructorException& e) {
+    std::cout << "Successfuly caught expected exception: Test 1 passed"
+              << std::endl;
+    return true;
+  }
+}
 
 /**
  * @brief Main Test Suite for ML Library
  * @return 0 if all tests succeed, somthing less than 0 otherwise
  */
 int main() {
-  int err_code = 0;
+  bool test_1 = test_vector_constructor();
+  bool all_tests_passed = test_1;
 
-  tensor_math::Matrix<int> test = tensor_math::Matrix<int>();
-  std::cout << test.GetRows() << std::endl;
-  std::cout << "Error Code: " << err_code << std::endl;
-  return 0;
+  if (all_tests_passed) {
+    std::cout << "All tests passed!" << std::endl;
+  } else {
+    std::cout << "Some tests failed..." << std::endl;
+  }
 }
